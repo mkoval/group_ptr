69e1dd85bfd5057ef3f64659c8dce15e6772dcd9
Michael Koval <mkoval@cs.cmu.edu>
2015-05-05 02:47:50 -0400

    Added a better example.

--- magic_ptr.cpp
+++ magic_ptr.cpp
@@ -1,70 +1,109 @@
 #include "group_ptr.hpp"
 #include <iostream>
+#include <vector>
 
-struct A {
-    A(std::string const &name)
+class BodyNode;
+
+class Skeleton {
+public:
+    virtual ~Skeleton()
+    {
+        std::cout << "-Skeleton(" << name_ << ")" << std::endl;
+    }
+
+    static group_ptr<Skeleton> create(std::string const &name)
+    {
+        group_ptr<Skeleton> p(new Skeleton(name));
+        // TODO: replace this with enable_shared_from_this equivalent
+        p->self_ = p;
+        return p;
+    }
+
+    group_ptr<BodyNode> add(group_ptr<BodyNode> const &node)
+    {
+        self_.lock().add_to_group(node);
+        nodes_.push_back(node);
+
+        // TODO: repeat this for the subtree rooted at node
+
+        return node;
+    }
+
+private:
+    std::string name_;
+    group_weak_ptr<Skeleton> self_;
+    std::vector<group_weak_ptr<BodyNode> > nodes_;
+
+    Skeleton(std::string const &name)
         : name_(name)
     {
-        std::cout << "+A(" << name_ << ")" << std::endl;
+        std::cout << "+Skeleton(" << name_ << ")" << std::endl;
     }
-
-    ~A()
-    {
-        std::cout << "-A(" << name_ << ")" << std::endl;
-    }
-
-    std::string name_;
 };
 
-struct B {
-    B(std::string const &name)
+
+class BodyNode {
+public:
+    ~BodyNode()
+    {
+        std::cout << "-BodyNode(" << name_ << ")" << std::endl;
+    }
+
+    static group_ptr<BodyNode> create(std::string const &name)
+    {
+        group_ptr<BodyNode> p(new BodyNode(name));
+        // TODO: replace this with enable_shared_from_this equivalent
+        p->self_ = p;
+        return p;
+    }
+
+private:
+    std::string name_;
+    group_weak_ptr<BodyNode> self_;
+
+    BodyNode(std::string const &name)
         : name_(name)
     {
-        std::cout << "+B(" << name_ << ")" << std::endl;
+        std::cout << "+BodyNode(" << name_ << ")" << std::endl;
     }
 
-    ~B()
-    {
-        std::cout << "-B(" << name_ << ")" << std::endl;
-    }
-
-    std::string name_;
+    friend class Skeleton;
 };
 
 int main(int argc, char **argv)
 {
-    std::cout << "+main" << std::endl;
+    std::cout << "Entering main" << std::endl;
+    group_ptr<BodyNode> r2d2_link3;
+
     {
-        auto g1a = make_group_ptr<A>("G1a");
+        std::cout << "Constructing R2D2 with three links." << std::endl;
+        group_ptr<Skeleton> r2d2 = Skeleton::create("r2d2");
+        r2d2->add(BodyNode::create("r2d2/link1"));
+        r2d2->add(BodyNode::create("r2d2/link2"));
+        r2d2_link3 = r2d2->add(BodyNode::create("r2d2/link3"));
 
-        std::cout << "+scope" << std::endl;
         {
-            auto g1b = make_group_ptr<A>("G1b");
-            auto g1c = make_group_ptr<A>("G1c");
-            g1a.add_to_group(g1b);
-            g1a.add_to_group(g1c);
-            g1c.reset(g1b);
+            std::cout << "Constructing C3P0 with four links." << std::endl;
+            group_ptr<Skeleton> c3p0 = Skeleton::create("c3p0");
+            c3p0->add(BodyNode::create("c3p0/link1"));
+            c3p0->add(BodyNode::create("c3p0/link2"));
+            c3p0->add(BodyNode::create("c3p0/link3"));
 
-            std::cout << "+scope" << std::endl;
-            {
-                auto g2a = make_group_ptr<B>("G2a");
-                auto g2b = make_group_ptr<B>("G2b");
-                g2a.add_to_group(g2b);
-                g1a.merge_group(g2a);
-            }
-            std::cout << "-scope" << std::endl;
+            group_ptr<BodyNode> c3p0_link4 = c3p0->add(BodyNode::create("c3p0/link4"));
+
+            std::cout << "Moving link4 from C3P0 to R2D2." << std::endl;
+            r2d2->add(c3p0_link4);
+
+            std::cout << "C3P0 is going out of scope..." << std::endl;
         }
 
-        group_weak_ptr<A> tmp1 = g1a;
-        group_weak_ptr<A const> tmp2 = tmp1;
-
-        group_ptr<A const> tmp2_lock = tmp2.lock();
-
-        //std::cout << "TESTING: " << tmp2.lock()->name_ << std::endl;
-
-        std::cout << "-scope" << std::endl;
+        std::cout << "R2D2 should not go out of scope because of link3" << std::endl;
     }
-    std::cout << "-main" << std::endl;
+
+    std::cout << "Also clearing link3, R2D2 is going out of scope..." << std::endl;
+    r2d2_link3.reset();
+
+    std::cout << "Leaving main" << std::endl;
 
     return 0;
 }
